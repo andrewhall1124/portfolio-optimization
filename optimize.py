896d97f812da4ad9b17d3235379dd611a1fea7a0
Andrew Hall <andrewmartinhall2@gmail.com>
2023-09-26 14:46:28 -0600

    redoing optimize.py

--- optimize.py
+++ optimize.py
@@ -1,85 +1,103 @@
-import pandas as pd
 import numpy as np
-from scipy import optimize
-import requests as rq
-import sys
-import configparser
-import time
+import pandas as pd
+from scipy import optimize 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+def maximize_that_fricking_sharpe_ratio(average_returns, covariance_matrix, risk_free, portfolio_size):
+    
+    # define maximization of Sharpe Ratio using principle of duality
+    def  f(x, average_returns, covariance_matrix, risk_free, portfolio_size):
+        funcDenomr = np.sqrt(np.matmul(np.matmul(x, covariance_matrix), x.T) )
+        funcNumer = np.matmul(np.array(average_returns),x.T)-risk_free
+        func = -(funcNumer / funcDenomr)
+        return func
 
-# Record the start time
-start_time = time.time()
+    #define equality constraint representing fully invested portfolio
+    def constraintEq(x):
+        A=np.ones(x.shape)
+        b=1
+        constraintVal = np.matmul(A,x.T)-b 
+        return constraintVal
+    
+    
+    #define bounds and other parameters
+    xinit=np.repeat(0.33, portfolio_size)
+    cons = ({'type': 'eq', 'fun':constraintEq})
+    lb = 0
+    ub = 1
+    bnds = tuple([(lb,ub) for x in xinit])
+    
+    #invoke minimize solver
+    opt = optimize.minimize (f, x0 = xinit, args = (average_returns, covariance_matrix,\
+                             risk_free, portfolio_size), method = 'SLSQP',  \
+                             bounds = bnds, constraints = cons, tol = 10**-3)
+    
+    return opt
 
-# initialize data
-tickers = ['ibm','aapl','meta', 'googl']
-weights = np.array([.25,.25,.25,.25])
-start_date = '2020-09-23 15:03:00'
-end_date = '2023-09-23 15:04:00'
-interval = '1day'
-risk_free = 5.46 / 100
-risk_free = (np.power((1 + risk_free),  (1.0 / 360.0)) - 1.0) * 100 
+## Fetching and setting up all of the data ##
 
-# Fetching stock data from 12 Data API
-API_KEY = config['API']['API_KEY']
-url = f"https://api.twelvedata.com/time_series?symbol={','.join(tickers)}&interval={interval}&start_date={start_date}&end_date={end_date}&apikey={API_KEY}"
-print(f"\nFetching stock data for \"{','.join(tickers)}\"")
-response = rq.get(url)
-stock_data_list = []
-if response.status_code == 200:
-  for ticker in tickers:
-    print(f"Parsing \"{ticker}\"")
-    data = response.json()
-    data = data[f"{ticker}"]['values']
-    data = pd.DataFrame(data)
-    data = data.sort_values(by="datetime", ascending=True)
-    data = data.reindex(columns=['datetime', 'close'])
-    data['close'] = pd.to_numeric(data['close'])
-    data['return'] = data['close'].pct_change() * 100
-    data['return'] = data['return'].fillna(0)
-    average_return = data['return'].mean()
-    std_return = data['return'].std()
-    returns = [float(x) for x in data['return'].values]
-    stock_data = {
-        "ticker": ticker,
-        "average_return": average_return,
-        "std_return": std_return,
-        "returns": returns
-    }
-    stock_data_list.append(stock_data)
-    print(f"Added \"{ticker}\" to stock data list")
-else:
-  print(f"Request failed with status code: {response.status_code}") 
+#Calculate average_returns and covariance_matrix
+stock_file_name = 'DJIA_Apr112014_Apr112019.csv'
+m = 1259  #excluding header
+columns = 29  #excluding date
 
-# Compute average_returns
-average_returns = np.array([stock['average_return'] for stock in stock_data_list])
+#Read in stock prices 
+df = pd.read_csv(stock_file_name,  nrows= m)
 
-# Calculate covariance matrix
-print("Calculating covariance matrix\n")
-returns_array = np.array([stock['returns'] for stock in stock_data_list])
-covariance_matrix = np.cov(returns_array)
+#Extract ticker symbols
+ticker_symbols = df.columns[1:columns+1].tolist()
 
-# Function for calculating portfolio return
-def calculate_portfolio_return(weights, average_returns):
-  return np.dot(weights,average_returns)
+#Remove timedate from stock data
+stock_data_df = df.iloc[0:, 1:]
 
-# Function for calculating portfolio variance
-def calculate_portfolio_std(weights, covariance_matrix):
-  return np.sqrt(np.dot(np.matmul(weights, covariance_matrix), weights))
+#Calculate stock returns matrix
+stock_prices_matrix = np.asarray(stock_data_df)
+[m, n]=stock_prices_matrix.shape
 
-#Function for calculating portfolio sharpe ratio
-def calculate_portfolio_sharpe(weights, average_returns, covariance_matrix):
-   return (calculate_portfolio_return(weights,average_returns) - risk_free) / calculate_portfolio_std(weights, covariance_matrix)
+def compute_stock_returns(stock_price, rows, columns):    
+    stock_return = np.zeros([rows-1, columns])
+    for j in range(columns):        # j: Stocks
+        for i in range(rows-1):     # i: Daily Prices
+            stock_return[i,j]=((stock_price[i+1, j]-stock_price[i,j])/stock_price[i,j])* 100
 
-print(f"Tickers: {tickers}")
-print(f"Weights: {weights}")
-print(f"Portfolio return: {calculate_portfolio_return(weights, average_returns)}")
-print(f"Risk free rate: {risk_free}")
-print(f"Portfolio variance: {calculate_portfolio_std(weights,covariance_matrix)}")
-print(f"Portfolio sharpe: {calculate_portfolio_sharpe(weights, average_returns, covariance_matrix)}")
+    return stock_return
 
-# Output elapsed time
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"\nExecuted in {elapsed_time} seconds\n")
+stock_returns_matrix = compute_stock_returns(stock_prices_matrix, m, n)
+
+#Calculate average_returns and covariance matrix
+average_returns = np.mean(stock_returns_matrix, axis = 0)
+covariance_matrix = np.cov(stock_returns_matrix, rowvar=False)
+
+## Now the fun begins ##
+
+#Initialize portfolio
+portfolio_size = columns
+annual_risk_free= 5.4 / 100
+daily_risk_free = (np.power((1 + annual_risk_free),  (1.0 / 360.0)) - 1.0) * 100 
+
+minRiskPoint = []
+expPortfolioReturnPoint =[]
+sharpe_ratio = 0
+
+#Compute maximal Sharpe Ratio and optimal weights
+result = maximize_that_fricking_sharpe_ratio(average_returns, covariance_matrix, daily_risk_free, portfolio_size)
+optimal_weights = np.array([result.x])
+
+#Compute other metrics  
+portfolio_risk = np.matmul((np.matmul(optimal_weights,covariance_matrix)), np.transpose(optimal_weights))
+portfolio_return = np.matmul(np.array(average_returns),optimal_weights.T)
+annualized_risk = np.sqrt(portfolio_risk*251) 
+annualize_return = 251*np.array(portfolio_return) 
+sharpe_ratio = (annualize_return-annual_risk_free)/annualized_risk 
+
+# Set precision for printing results
+np.set_printoptions(precision=3, suppress = True)
+
+# Display results
+print('Maximal Sharpe Ratio:  ', sharpe_ratio)
+print('\nAnnualized Risk (%):  ', annualized_risk)
+print('\nAnnualized Expected Portfolio Return (%):  ', annualize_return)
+print('\nOptimal weights (%):')
+
+# Print the optimal weights with the ticker symbol
+for i in range(len(ticker_symbols)):
+  print('{} | {:.2f}'.format(ticker_symbols[i], optimal_weights[0][i] * 100))
